c3ac00e9b9a242f373d1153cb8694dc89ae8e84c
Larry Lade <lplade@users.noreply.github.com>
2016-03-27 21:05:00 -0500

    Tweaking gen script

--- scripts/gen_audio.sh
+++ scripts/gen_audio.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+# Requires:
+# sox, libsox-fmt-mp3
+#
+# Convert source file to AIFF before using. MP3 gets sync errors
+AMBIENT="audio_src/Silent-Crypt_-3db.aiff"
+ARISE="audio_src/arise2.aiff"
 
-AMBIENT="al_cv_cryptamb1-10+mins_quieter.aiff"
-ARISE="arise2.aiff"
+#TODO escape out any spaces in above filenames
+#TODO text for sox w/ mp3 support
+#TODO test if these files exist
+#TODO show progress as files are converted
+#TODO trap errors
 
+# Sum of trim start and trim duration cannot exceed length of AMBIENT
 
-sox -m "|sox $AMBIENT -p trim 0 11" "|sox $ARISE -p pad 5" 005_day.aiff
-sox -m "|sox $AMBIENT -p trim 0 16" "|sox $ARISE -p pad 10" 010_week.aiff
-sox -m "|sox $AMBIENT -p trim 0 26" "|sox $ARISE -p pad 20" 020_month.aiff
-sox -m "|sox $AMBIENT -p trim 0 46" "|sox $ARISE -p pad 40" 040_year.aiff
-sox -m "|sox $AMBIENT -p trim 0 66" "|sox $ARISE -p pad 60" 060_decade.aiff
-sox -m "|sox $AMBIENT -p trim 0 126" "|sox $ARISE -p pad 120" 120_century.aiff
-sox -m "|sox $AMBIENT -p trim 0 186" "|sox $ARISE -p pad 180" 180_centuries.aiff
-sox -m "|sox $AMBIENT -p trim 0 246" "|sox $ARISE -p pad 240" 240_millenium.aiff
-sox -m "|sox $AMBIENT -p trim 0 306" "|sox $ARISE -p pad 300" 300_age.aiff
-sox -m "|sox $AMBIENT -p trim 0 366" "|sox $ARISE -p pad 360" 360_epoch.aiff
+sox -V3 "|sox $AMBIENT -p trim 0 5" $ARISE -C -4.2 audio/005_day.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 5 10" $ARISE -C -4.2 audio/010_week.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 15 20" $ARISE -C -4.2 audio/020_month.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 35 40" $ARISE -C -4.2 audio/040_year.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 75 60" $ARISE -C -4.2 audio/060_decade.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 135 120" $ARISE -C -4.2 audio/120_century.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 255 180" $ARISE -C -4.2 audio/180_centuries.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 0 240" $ARISE -C -4.2 audio/240_millenium.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 240 300" $ARISE -C -4.2 audio/300_age.mp3 splice
+sox -V3 "|sox $AMBIENT -p trim 540 360" $ARISE -C -4.2 audio/360_epoch.mp3 splice
+
+#for i in audio/*.aiff
+#do
+#  lame "$i"
+#done
+
+#rm audio/*.aiff
+
+exit 0
